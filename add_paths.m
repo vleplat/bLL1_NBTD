2fdf5addfab1ccd968f365dc56c3ef3fc1adc3dd
vleplat <87977896+vleplat@users.noreply.github.com>
2022-12-05 08:55:35 +0300

    update loading paths script

--- add_paths.m
+++ add_paths.m
@@ -1,3 +1,7 @@
+% add paths
+addpath data\
+addpath demos\
+addpath src\
 addpath libraries\tensorlab_2016_03_28\
 addpath(genpath('libraries\HSMSFusionToolbox'))
 addpath(genpath('libraries\TensorFusionToolbox'))
